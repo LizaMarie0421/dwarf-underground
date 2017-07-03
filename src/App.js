39b57a84a622ced85e9f32596650b45259c9c57a
LizaMarie <lizamariejimenez@gmail.com>
2017-07-03 13:43:12 -0500

    added components: for article (article body, article links, avatar, column, and footer)

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './Header';
 import Article from './Article';
 import Column from './Column';
 import OtherArticles from './OtherArticles';
+import Footer from './Footer';
 import './App.css';
 
 class App extends Component {
@@ -15,11 +16,6 @@ class App extends Component {
       <Column/>
       <OtherArticles/>
     </main>
-
-    <footer className="expanded row">
-      <h6>&copy; 2017 Erebor Industries</h6>
-      <h6 className="float-right">All rights reserved</h6>
-    </footer>
     </div>
     );
   }
