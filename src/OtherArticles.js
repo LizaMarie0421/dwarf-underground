09ed19f6f7e7cc9a96d5de316777fcba1dda7643
LizaMarie <lizamariejimenez@gmail.com>
2017-07-03 13:07:33 -0500

    Added article component, other articles component with other article (singular) component in that component

---
+++ src/OtherArticles.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './OtherArticles.css'
+import OtherArticle from './OtherArticle';
+class OtherArticles extends Component {
+	render(){
+return (  
+  <div className="small-12 columns other-articles">
+        <h2>From around the Realm</h2>
+<OtherArticle/>
+    </div>   
+)
+}
+}
+export default OtherArticles
